5593cbbc6c5a626871c87873626a449c4ce57549
Mark Berman <mark@powwow.audio>
2018-02-24 18:35:05 +0100

    Tutorial 1:Serialization

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,16 +15,17 @@ Including another URLconf
 """
 
 from django.urls import include, path
-from rest_framework import routers
-from tutorial.quickstart import views
-from django.contrib import admin
+# from rest_framework import routers
+# from tutorial.quickstart import views
+# from django.contrib import admin
 
-router = routers.DefaultRouter()
-router.register('users', views.UserViewSet)
-router.register('groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register('users', views.UserViewSet)
+# router.register('groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
+    path('', include('snippets.urls')),
+    #    path('', include(router.urls)),
+    #    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    #    path('admin/', admin.site.urls),
 ]
