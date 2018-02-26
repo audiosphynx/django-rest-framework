85bc07e982734753b2915628a97ff695504534bc
Mark Berman <mark@powwow.audio>
2018-02-26 15:01:42 +0100

    Tutorial 6: ViewSets & Routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,13 +1,24 @@
 from django.urls import include, path
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
+# Create a router and register our viewsets with it.
+# Using viewsets is less explicit than building your views individually.
+router = DefaultRouter()
+router.register('snippets', views.SnippetViewSet)
+router.register('users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls))
+]
+
 # API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('snippets/<pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<pk>/', views.UserDetail.as_view(), name='user-detail')
-])
+# urlpatterns = format_suffix_patterns([
+#    path('', api_root),
+#    path('snippets/', snippet_list, name='snippet-list'),
+#    path('snippets/<pk>/', snippet_detail, name='snippet-detail'),
+#    path('snippets/<pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+#    path('users/', user_list, name='user-list'),
+#    path('users/<pk>/', user_detail, name='user-detail')
+# ])
